9a5d952fa6c176971af13c10c7cdb6cfca465d09
Peter Grahams <deanwellsg@gmail.com>
2021-01-21 06:29:44 +0300

    updated StudentRecords.java

--- src/com/recordstudent/Main.java
+++ src/com/recordstudent/Main.java
@@ -2,7 +2,10 @@ package com.recordstudent;
 
 public class Main {
     public static void main(String[] args) {
-        //objects of the class referencing the class
+        //objects of the class referencing the class studentRecords
+        StudentRecords studentRecords = new StudentRecords(); // Referencing to class StudentRecords
+
+
 
     }
 
