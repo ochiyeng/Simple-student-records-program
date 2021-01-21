46d5d5b1a6394eff2b857f5ca2df347ddc12910f
Peter Grahams <deanwellsg@gmail.com>
2021-01-21 06:50:02 +0300

    updated StudentRecords.java

--- src/com/recordstudent/StudentRecords.java
+++ src/com/recordstudent/StudentRecords.java
@@ -6,4 +6,36 @@ public class StudentRecords {
      String name;
      int age;
      char gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
 }
