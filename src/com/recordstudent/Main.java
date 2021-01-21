3846252d9eeca36d97322d9180577e0ed1bcd249
Peter Grahams <deanwellsg@gmail.com>
2021-01-21 06:44:51 +0300

    updated StudentRecords.java

--- src/com/recordstudent/Main.java
+++ src/com/recordstudent/Main.java
@@ -3,8 +3,11 @@ package com.recordstudent;
 public class Main {
     public static void main(String[] args) {
         //objects of the class referencing the class studentRecords
-        StudentRecords studentRecords1;  //Referencing to class StudentRecords
-        studentRecords1 = new StudentRecords(); // Allocating memory to the StudentsRecords
+        StudentRecords studentRecordsFirstEntry;  //Referencing to class StudentRecords
+        studentRecordsFirstEntry = new StudentRecords(); // Allocating memory to the StudentsRecords
+        StudentRecords studentRecordsSecondEntry = new StudentRecords();
+
+        //Assigning values to studentRecordsFirstEntry
 
 
 
